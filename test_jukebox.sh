1c64827316f7bcbdacc3a8bef3be08a5077968bf
howff <howff>
2021-01-04 12:07:29 +0000

    test two locations

--- test_jukebox.sh
+++ test_jukebox.sh
@@ -2,6 +2,12 @@
 
 url="http://localhost:8123"
 
+echo "dua lipa in kitchen..."
 curl -X POST -H 'Content-type: application/json' -d '{"command":"jukebox","parameter":"play dua lipa in kitchen"}' "$url"
 sleep 5
-curl -X POST -H 'Content-type: application/json' -d '{"command":"jukebox","parameter":"pause"}' "$url"
+echo "queen in office..."
+curl -X POST -H 'Content-type: application/json' -d '{"command":"jukebox","parameter":"play queen in office"}' "$url"
+sleep 5
+echo "pause..."
+curl -X POST -H 'Content-type: application/json' -d '{"command":"jukebox","parameter":"pause in kitchen"}' "$url"
+curl -X POST -H 'Content-type: application/json' -d '{"command":"jukebox","parameter":"pause in office"}' "$url"
